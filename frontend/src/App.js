7f981f0410e53a0c297a7fbc3f1ab28a495841a3
Muhammed Besirovic <besirovicmm@gmail.com>
2021-04-20 18:17:50 +0200

    day1.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+import { Container } from 'react-bootstrap';
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome</h1>
-    </>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
