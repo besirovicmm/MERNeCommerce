833a87babcd5c17858bc188b82ca929d1868d068
Muhammed Besirovic <besirovicmm@gmail.com>
2021-04-22 15:25:49 +0200

    day2

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,64 @@
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            value >= 1
+              ? 'fa fa-star'
+              : value >= 0.5
+              ? 'fa fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 2
+              ? 'fa fa-star'
+              : value >= 1.5
+              ? 'fa fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 3
+              ? 'fa fa-star'
+              : value >= 2.5
+              ? 'fa fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 4
+              ? 'fa fa-star'
+              : value >= 3.5
+              ? 'fa fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 5
+              ? 'fa fa-star'
+              : value >= 4.5
+              ? 'fa fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
